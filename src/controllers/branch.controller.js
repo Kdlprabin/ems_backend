91859fd1e74f177ab3be3be395a9636330428e22
kdlprabin <prabinkandel444@gmail.com>
2025-08-18 21:17:31 +0545

    branches api fixed

--- src/controllers/branch.controller.js
+++ src/controllers/branch.controller.js
@@ -3,23 +3,7 @@ const Data = require('../models/data.model');
 const getBranch = async (req, res) => {
     try {
         //fetch data having unique branch name
-        const branches = await Data.aggregate([
-            {
-                $group: {
-                    _id: "$Branch",
-                    data: { $first: "$$ROOT" }
-                }
-            },
-            {
-                $project: {
-                    _id: 0,
-                    Branch: "$_id",
-                    Category: "$data.Category",
-                    "Previous Years": "$data['Previous Years']",
-                    "Current Year": "$data['Current Year']"
-                }
-            }
-        ]); 
+        const branches = await Data.distinct("Branch")
 
         return res.status(200).json(branches);
     }catch(err){
