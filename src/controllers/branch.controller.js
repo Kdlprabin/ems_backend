d372620b61f1609576c3de256e7373df714994ff
kdlprabin <prabinkandel444@gmail.com>
2025-08-18 23:32:38 +0545

    all in dashboard

--- src/controllers/branch.controller.js
+++ src/controllers/branch.controller.js
@@ -5,7 +5,10 @@ const getBranch = async (req, res) => {
         //fetch data having unique branch name
         const branches = await Data.distinct("Branch")
 
-        return res.status(200).json(branches);
+        return res.status(200).json([...branches, {
+            _id: Math.random(),
+            branch_name: "All"
+        }]);
     }catch(err){
         console.error(err);
         return res.status(500).json({ error: "Failed to fetch branch data" });
